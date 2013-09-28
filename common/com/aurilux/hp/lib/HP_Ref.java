5478e421481f5c48a3f347809381c56f4f793240
Aurilux <primordias@gmail.com>
2013-09-28 15:10:31 -0500

    Added mcmod.info, pack.mcmeta and a few changes to build files

--- common/com/aurilux/hp/lib/HP_Ref.java
+++ common/com/aurilux/hp/lib/HP_Ref.java
@@ -3,5 +3,5 @@ package com.aurilux.hp.lib;
 public class HP_Ref {
 	public static final String MOD_ID =   "@MOD_ID@";
 	public static final String MOD_NAME = "@MOD_NAME@";
-	public static final String VERSION =  "@VERSION@ (build @BUILD_NUMBER@)";
+	public static final String VERSION =  "@MOD_VERSION@ (build @BUILD_NUMBER@)";
 }
